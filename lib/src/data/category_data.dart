db2aaf6a9f2d2cfa293acb7fca9add4af254da57
abhinavprkash <51023991+abhinavprkash@users.noreply.github.com>
2020-04-04 14:43:16 +0530

    1st commit

---
+++ lib/src/data/category_data.dart
@@ -0,0 +1,44 @@
+import '../models/category_model.dart';
+
+final categories = [
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/onion.jpg",
+      categoryName: "Onion"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/cabbage.jpg",
+      categoryName: "cabbage"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/beetroot.jpg",
+      categoryName: "beetroot"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/cucumber.jpg",
+      categoryName: "cucumber"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/frenchring.jpg",
+      categoryName: "frencbeans"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/garlic.jpg",
+      categoryName: "garlic"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/ginger.jpg",
+      categoryName: "ginger"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/ladiesfinger.jpg",
+      categoryName: "ladiesfinger"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/potato.jpg",
+      categoryName: "potato"),
+  Category(
+      numberOfItems: 43,
+      imagePath: "assets/images/tomato.jpg",
+      categoryName: "tomato"),
+];
---
+++ lib/src/homescreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:vegdy/src/widgets/food_category.dart';
+import 'widgets/home_top_info.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final textStyle = TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+        children: <Widget>[
+          HomeTopInfo(),
+          FoodCategory(),
+        ],
+      ),
+    );
+  }
+}
