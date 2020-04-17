4917716436cb4f3afc699579cea9f2483174f952
abhinavprkash <51023991+abhinavprkash@users.noreply.github.com>
2020-04-17 19:05:35 +0530

    profile page

--- lib/src/pages/profile_page.dart
+++ lib/src/pages/profile_page.dart
@@ -8,8 +8,33 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile Page"),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 50.0),
+        child: Column(
+          children: <Widget>[
+            Text("Profile"),
+            Row(
+              children: <Widget>[
+                Container(
+                  height: 120.0,
+                  width: 120.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60.0),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 3.0,
+                        offset: Offset(0, 4.0),
+                        color: Colors.black38,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
